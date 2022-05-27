1811da65da46ddce2f6706014991bd67f10b2718
Tate Thurston <tatethurston@gmail.com>
2022-05-27 14:26:59 -0700

    add encode/decode runtime for json bytes

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export { BinaryReader } from "./runtime/reader";
 export { BinaryWriter } from "./runtime/writer";
+export { decodeBase64Bytes, encodeBase64Bytes } from "./runtime/json";
---
+++ src/runtime/json.ts
@@ -0,0 +1,9 @@
+import { byteArrayToString, stringToUint8Array } from "./goog/crypt";
+
+export function encodeBase64Bytes(bytes: Uint8Array): string {
+  return btoa(byteArrayToString(bytes));
+}
+
+export function decodeBase64Bytes(bytes: string): Uint8Array {
+  return stringToUint8Array(atob(bytes));
+}
