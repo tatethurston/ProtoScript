7a933bb762588181169f94becf1467da1601c01a
Tate Thurston <tatethurston@gmail.com>
2022-07-06 13:48:29 -0700

    Update index.js

--- examples/protoc/src/index.js
+++ examples/protoc/src/index.js
@@ -1,7 +1,6 @@
 const { Hat } = require("./haberdasher_pb");
 
 console.info("Protobuf");
-
 const msg = new Hat([3, "red", "top hat"]).serializeBinary();
 console.log("serialized:", msg);
 const hat = Hat.deserializeBinary(msg);
