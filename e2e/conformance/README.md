515459781b9964b42fbc646ed2707dc573875a91
Tate Thurston <tatethurston@gmail.com>
2023-06-21 08:51:21 -0700

    document conformance setup (#33)

--- e2e/conformance/README.md
+++ e2e/conformance/README.md
@@ -1,3 +1,9 @@
 Tests for `test_messages_proto2.proto` are intentionally skipped because ProtoScript targets Protocol Buffers v3.
 
 ProtoScript does not implement support for `group`s, and will throw when encountering `group` in protocol buffer messages. Groups are deprecated by protocol buffers and [should not be used](https://protobuf.dev/programming-guides/proto2/#groups).
+
+The conformance test runner is built with `pnpm conformance:build`. `test.ts` is a jest file that executes the conformance test using the `runner.ts` runner.
+
+### Dependencies
+
+`brew install bazel`
--- e2e/conformance/bin/generate_conformance_test_runner.sh
+++ e2e/conformance/bin/generate_conformance_test_runner.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 ###
 # Run from e2e/conformance:
 # ./bin/generate_conformance_test_runner
@@ -5,11 +6,14 @@
 TMP="tmp"
 GOOGLE_PROTOBUF_VERSION=23.3
 
-mkdir -p bin
+mkdir -p "$TMP"
+
 curl -L https://github.com/protocolbuffers/protobuf/releases/download/v"$GOOGLE_PROTOBUF_VERSION"/protobuf-"$GOOGLE_PROTOBUF_VERSION".tar.gz >|"$TMP"/protobuf-"$GOOGLE_PROTOBUF_VERSION".tar.gz
 tar -xzf "$TMP"/protobuf-"$GOOGLE_PROTOBUF_VERSION".tar.gz -C "$TMP"/
-Vjj
+
 pushd "$TMP"/protobuf-"$GOOGLE_PROTOBUF_VERSION"
 bazel build test_messages_proto3_cc_proto conformance:conformance_proto conformance:conformance_test conformance:conformance_test_runner
 cp bazel-bin/conformance/conformance_test_runner ../../bin/conformance_test_runner
 popd
+
+rm -r "$TMP"
