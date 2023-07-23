ac3737eb613a4181556fcaeb22c12f6876012d22
Tate Thurston <tatethurston@gmail.com>
2023-07-23 16:47:13 -0700

    package refactor

--- e2e/treeshaking/TreeshakingTestJSON.ts
+++ e2e/treeshaking/TreeshakingTestJSON.ts
@@ -1,3 +1,3 @@
-import { TreeshakingTestJSON } from "./treeshaking.pb";
+import { TreeshakingTestJSON } from "./treeshaking.pb.js";
 
 export default TreeshakingTestJSON.initialize();
--- packages/protoscript/src/runtime/index.ts
+++ packages/protoscript/src/runtime/index.ts
@@ -4,10 +4,7 @@ export { decodeBase64Bytes, encodeBase64Bytes } from "./json.js";
 export type ByteSource = ArrayBuffer | Uint8Array | number[] | string;
 export type PartialDeep<T> = {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  [P in keyof T]?: T[P] extends any[]
-    ? T[P]
-    : // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    T[P] extends Record<any, any>
+  [P in keyof T]?: T[P] extends any[] | Record<any, any> | Uint8Array
     ? T[P]
     : T[P] extends object | null | undefined
     ? PartialDeep<T[P]>
